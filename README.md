c209f95e1fffbcbbaa4637a3a911228ea5583b53
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2022-06-22 17:39:16 +0900

    practice

---
+++ README.md
@@ -0,0 +1,2 @@
+# my_blog_v1
+- myblog 프로젝트
---
+++ jpa_basic_crud_pr/src/main/java/com/example/jpa_basic_crud_pr/JpaBasicCrudPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_basic_crud_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicCrudPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBasicCrudPrApplication.class, args);
+	}
+
+}
